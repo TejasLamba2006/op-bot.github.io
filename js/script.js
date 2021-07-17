95bf97a446e4303e963595b68e4470a9a8c25fa0
Tejas Lamba <73745640+TajuModding@users.noreply.github.com>
2021-07-17 18:23:48 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,3 +17,19 @@
     e.forEach(a => {
       if (window.mobilecheck()) a.style.paddingBottom = "4px";
     })
+const options = {
+  bottom: '64px', // default: '32px'
+  right: 'unset', // if you want the widget to be on the right then `right: '32px' and left to unset
+  left: '32px', // default: 'unset'
+  time: '0.5s', // default: '0.5s' the time to fade in and out 
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: false, // set to true if you want to load the site in the mode the user left it in
+  label: '🌓', // the emoji you want for toggling light and dark mode
+  autoMatchOsTheme: true // if you have enabled dark mode it would open darkmode
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
